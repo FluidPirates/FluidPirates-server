50360a10e27c228287a70888eec8a7a3026ac448
Dorian <dorian@doma.io>
2015-12-14 15:39:19 +0100

    Design the API's routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,30 @@
 Rails.application.routes.draw do
-  namespace 'api', defaults: { format: :json }, constraints: { format: :json } do
-    post 'sessions' => 'sessions#create'
-    delete 'sessions' => 'sessions#destroy'
+  namespace "api", defaults: { format: :json }, constraints: { format: :json } do
+    post "sessions" => "sessions#create"
+    delete "sessions" => "sessions#destroy"
 
-    post 'users' => 'users#create'
-    get 'users/current' => 'users#current'
+    post "users" => "users#create"
+    get "users/current" => "users#current"
+
+    resources :groups do
+      get "users" => "groups#users"
+
+      resources :categories do
+        get "polls" => "polls#index"
+
+        resources "delegations"
+      end
+
+      resources :polls do
+        resources :propositions do
+          resources :choices do
+            resources :votes
+          end
+        end
+      end
+    end
   end
 
-  root 'pages#home'
-  get '*path' => 'pages#not_found', format: 'html'
+  root "pages#home"
+  get "*path" => "pages#not_found", format: "html"
 end
