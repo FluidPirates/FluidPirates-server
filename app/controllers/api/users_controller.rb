7d3fe15556d4f79a77e724bc2399ec943f3dd625
Dorian <dorian@doma.io>
2015-12-14 15:41:13 +0100

    add registration action

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,9 +1,27 @@
 class API::UsersController < API::APIController
+  skip_before_action :verify_token, only: [:create]
+
   def current
     if current_user
       render json: current_user.to_json(only: [:id, :name, :email])
     else
       render json: {}
     end
+  end
+
+  def create
+    @user = User.create(user_params)
+
+    if @user.save
+      render json: { token: Token.create_for_user(@user), message: "Signed in" }
+    else
+      render_error(@user.errors.full_messages.join(", "))
+    end
+  end
+
+  protected
+
+  def user_params
+    params[:user].try(:permit, [:name, :email, :password])
   end
 end
